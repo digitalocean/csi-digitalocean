e04e67d2c1dfe7c08a1610624456c359e2d81c67
Fatih Arslan <ftharsln@gmail.com>
2018-08-29 15:55:02 +0300

    driver: add ready flag to `Probe()`

--- driver/identity.go
+++ driver/identity.go
@@ -20,6 +20,7 @@ import (
 	"context"
 
 	csi "github.com/container-storage-interface/spec/lib/go/csi/v0"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/sirupsen/logrus"
 )
 
@@ -61,5 +62,12 @@ func (d *Driver) GetPluginCapabilities(ctx context.Context, req *csi.GetPluginCa
 // Probe returns the health and readiness of the plugin
 func (d *Driver) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.ProbeResponse, error) {
 	d.log.WithField("method", "prove").Info("probe called")
-	return &csi.ProbeResponse{}, nil
+	d.readyMu.Lock()
+	defer d.readyMu.Unlock()
+
+	return &csi.ProbeResponse{
+		Ready: &wrappers.BoolValue{
+			Value: d.ready,
+		},
+	}, nil
 }
