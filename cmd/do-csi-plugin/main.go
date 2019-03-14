13998e36f0f4daa922ab26376d21529ff4a74aa1
Cody Baker <cody@codybaker.com>
2019-03-14 10:55:28 -0400

    Tag volumes on Create/Attach (#130)
    * tag DO volumes on create/attach

--- cmd/do-csi-plugin/main.go
+++ cmd/do-csi-plugin/main.go
@@ -30,6 +30,7 @@ func main() {
 		endpoint = flag.String("endpoint", "unix:///var/lib/kubelet/plugins/"+driver.DriverName+"/csi.sock", "CSI endpoint")
 		token    = flag.String("token", "", "DigitalOcean access token")
 		url      = flag.String("url", "https://api.digitalocean.com/", "DigitalOcean API URL")
+		doTag    = flag.String("do-tag", "", "Tag DigitalOcean volumes on Create/Attach")
 		version  = flag.Bool("version", false, "Print the version and exit.")
 	)
 	flag.Parse()
@@ -39,7 +40,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	drv, err := driver.NewDriver(*endpoint, *token, *url)
+	drv, err := driver.NewDriver(*endpoint, *token, *url, *doTag)
 	if err != nil {
 		log.Fatalln(err)
 	}
