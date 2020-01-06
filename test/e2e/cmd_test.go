445a11ac99c9b2198c9c7e546e1943ed43bac5f0
Timo Reimann <treimann@digitalocean.com>
2020-01-06 21:27:33 +0100

    Use Cmd.Std{out,err}

--- test/e2e/cmd_test.go
+++ test/e2e/cmd_test.go
@@ -2,12 +2,8 @@ package e2e
 
 import (
 	"context"
-	"fmt"
-	"io"
 	"os"
 	"os/exec"
-
-	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 )
 
 type cmdParams struct {
@@ -23,46 +19,8 @@ func runCommand(ctx context.Context, name string, params cmdParams) error {
 	cmd.Env = append(os.Environ(), params.envs...)
 	cmd.Dir = params.dir
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return fmt.Errorf("failed to get stdout pipe: %s", err)
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return fmt.Errorf("failed to get stderr pipe: %s", err)
-	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-	err = cmd.Start()
-	if err != nil {
-		return fmt.Errorf("failed to start command: %s", err)
-	}
-
-	// Copy stdout/stderr output while the command is running. The io.Copy
-	// operations will terminate once the command finishes.
-	err = utilerrors.AggregateGoroutines(
-		func() error {
-			_, err := io.Copy(os.Stdout, stdout)
-			if err != nil {
-				return fmt.Errorf("failed to copy from stdout pipe: %s", err)
-			}
-			return nil
-		},
-		func() error {
-			_, err := io.Copy(os.Stderr, stderr)
-			if err != nil {
-				return fmt.Errorf("failed to copy from stderr pipe: %s", err)
-			}
-			return nil
-		},
-	)
-	if err != nil {
-		return fmt.Errorf("failed to copy output: %s", err)
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		return fmt.Errorf("failed to wait for command: %s", err)
-	}
-
-	return nil
+	return cmd.Run()
 }
