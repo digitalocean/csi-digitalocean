1403c69b0b8256378b6a124ddd576c8802d754a5
Timo Reimann <treimann@digitalocean.com>
2022-01-05 10:56:28 +0100

    Update headers to 2022

--- scripts/get-e2etest-sha256.sh
+++ scripts/get-e2etest-sha256.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2020 DigitalOcean
+# Copyright 2022 DigitalOcean
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
