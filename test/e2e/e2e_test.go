cb7cda3269abfa26e12fa770e4449e2636b8e355
Oliver Love <olove@digitalocean.com>
2023-08-17 11:08:50 -0400

    CON-9410 update csi

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -54,7 +54,7 @@ var (
 	errTokenMissing = errors.New("token must be specified in DIGITALOCEAN_ACCESS_TOKEN environment variable")
 
 	// De-facto global variables that require initialization at runtime.
-	supportedKubernetesVersions     = []string{"1.27", "1.26", "1.25", "1.24"}
+	supportedKubernetesVersions     = []string{"1.28", "1.27", "1.26", "1.25"}
 	sourceFileDir                   string
 	testdriverDirectoryAbsolutePath string
 	deployScriptPath                string
