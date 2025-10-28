30bbfa9c810dc6414434442e2fe8ee5a1ee008bb
Oliver Love <olove@digitalocean.com>
2025-10-28 13:50:21 -0400

    CON-12569 fix

--- test/e2e/e2e.sh
+++ test/e2e/e2e.sh
@@ -21,8 +21,7 @@ set -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 TIMEOUT="${TIMEOUT:-1h}"
-DOCKER_API_VERSION=1.51
 (
   cd "${SCRIPT_DIR}"
-  DEBUG_E2E="${DEBUG_E2E:-}" go test -v -tags e2e -test.timeout "${TIMEOUT}" ./... -args -long "$@"
+  DOCKER_API_VERSION=1.48 DEBUG_E2E="${DEBUG_E2E:-}" go test -v -tags e2e -test.timeout "${TIMEOUT}" ./... -args -long "$@"
 )
